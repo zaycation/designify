3b62caeef08a7bc74d735e2526f4f103ce02d06e
zaycation.dev <isaiahthomas098@gmail.com>
2021-04-22 15:50:13 -0700

    updated with sitemap

---
+++ src/sitemap-routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router";
+
+export default (
+  <Route>
+    <Route path="/" />
+    <Route path="/home" />
+    <Route path="/form" />
+    <Route path="/examples" />
+    <Route path="/services" />
+    <Route path="/pricing" />
+  </Route>
+);
